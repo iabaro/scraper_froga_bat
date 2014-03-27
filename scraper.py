2ecd717e93aa9d6479170da729f34e54294e3e77
Iñaki Arrieta Baro <dokumentazioa@euskomedia.org>
2014-03-27 19:08:15 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,21 @@
 # This is a template for a Python scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+
+url = "http://www.euskomedia.org/aunamendi/5601"
+html = scraperwiki.scrape(url)
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
+root = lxml.html.fromstring(html)
+izenburua = root.cssselect("h1")
+
+
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['url'], data={url: izenburua})
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
